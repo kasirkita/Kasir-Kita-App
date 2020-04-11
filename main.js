2ee36910bb84101a12b9454d1eff2c4f99f9c0b0
arka.yulianto <yulianto.saparudin@arkamaya.co.id>
2020-04-11 22:33:29 +0700

    :bug: fix

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const express = require("./server");
-const electron = require("electron");
+const {app, Menu, BrowserWindow} = require("electron");
 const path = require('path');
 const iconPath = path.join(__dirname, "build", "icon.png");
-
-const app = electron.app;
-const BrowserWindow  = electron.BrowserWindow;
 
 let mainWindow;
 
@@ -33,3 +30,85 @@ app.on("activate", () => {
         createWindow();
     }
 });
+
+const isMac = process.platform === 'darwin'
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { 
+        label: 'Tentang',
+        role: 'about'
+        },
+      { type: 'separator' },
+      { 
+          label: 'Keluar',
+          role: 'quit'
+        }
+    ]
+  }] : []),
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { label: 'Tutup', role: 'close' } : { label: 'Keluar', role: 'quit' },
+      { 
+        label: 'Muat Ulang',
+        role: 'reload'
+     },
+    ]
+  },
+  {
+    label: 'Tentang',
+    role: 'help',
+    submenu: [
+    {
+        label: 'URL Backend',
+        click: async () => {
+            const prompt = require('electron-prompt');
+
+            const url = await mainWindow.webContents.executeJavaScript('localStorage.getItem("url");', true)
+              .then((result) => {
+                return result 
+
+            })
+            
+            prompt({
+                title: 'URL Backend',
+                label: 'Silahkan masukan URL Backend:',
+                value: url,
+                inputAttrs: {
+                    type: 'url'
+                },
+                type: 'input'
+            })
+            .then((r) => {
+                if(r === null) {
+                    console.log('user cancelled');
+                } else {
+
+                    mainWindow.webContents.executeJavaScript(`localStorage.setItem("url", "${r}");`, true)
+                        .then((result) => { 
+                            mainWindow.loadURL("http://localhost:3002")
+                    }).catch(err => {
+                        console.log(err)
+                    })
+                }
+            })
+            .catch(console.error);
+        }
+    },
+      {
+        label: 'Bantuan',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://github.com/kasirkita/Kasir-Kita')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
